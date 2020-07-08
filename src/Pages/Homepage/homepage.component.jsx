c98bdee3bf3e8c7c82699aeb42a87eab186d74dd
sumitsssss <sumitsinhassss@outlook.com>
2020-07-08 23:00:39 +0530

    Added HomePage Component

--- src/Pages/Homepage/homepage.component.jsx
+++ src/Pages/Homepage/homepage.component.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import './homepage.style.scss';
-import DirectoryHook from '../../Components/Directory/directory.component.hook';
+import Directory from '../../Components/Directory/directory.component'
 
 export default function HomePage() {
   return (
     <div className="homepage">
-      <DirectoryHook/>
+      <Directory/>
     </div>
   );
 }
