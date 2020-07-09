153eaa3c63fec5bbd326baa5c48870f2155917a6
sumitsssss <sumitsinhassss@outlook.com>
2020-07-09 18:47:41 +0530

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import Homepage from './Pages/Homepage/homepage.component'
+import React from "react";
+import Homepage from "./Pages/Homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/Pages/Homepage/homepage.component.jsx
+++ src/Pages/Homepage/homepage.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './homepage.style.scss';
+
 import Directory from '../../Components/Directory/directory.component'
 
 export default function HomePage() {
