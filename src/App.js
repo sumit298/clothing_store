4241109fcdc6376777a92df70a84f774e1d9ec88
sumitsssss <sumitsinhassss@outlook.com>
2020-07-12 11:46:28 +0530

    Added Header Component in Hooks

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Homepage from "./Pages/Homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./Pages/ShopPage/shopPage.component";
+import Header from "./Components/Header/header.components";
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route path="/" component={Homepage} exact />
         <Route path="/shop" component={ShopPage}/>
