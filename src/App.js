638b0e05dbb24e2415123df8c8f9171fa4ab7327
sumitsssss <sumitsinhassss@outlook.com>
2020-07-13 19:39:52 +0530

    Added Header Component Master

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Homepage from "./Pages/Homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from './Pages/Shop/shop.component'
+import Header from "./Components/Header/header.component";
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
