d1817b37b15acc6d498f636c7d5608df3a99aaff
Sumit <sumitsinhassss@outlook.com>
2020-09-06 10:29:46 +0530

    Revert "Merge branch 'Redux_Old' into Hook"
    This reverts commit cd5f40dcaedb38ca2808abfecb6ab01121e2aafc.

--- src/Redux/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-// import { useReducer } from "reducer";
-
-
-export default combineReducers({
-   
-})
--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import Store from './Redux/Store';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Provider store={Store}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</Provider>
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
